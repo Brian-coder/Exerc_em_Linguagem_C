a6f8886ef319ed27f86f2114656e100a89484496
Brian Rennó <52013757+Brian-coder@users.noreply.github.com>
2019-07-29 18:34:41 -0300

    Update Exercício 2.c

--- Exercício 2.c
+++ Exercício 2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Função principal do programa
 void main (){
 
+//Imprime na tela 
 printf("\n    *   ");
 printf("\n   ***  ");
 printf("\n  ***** ");
 printf("\n *******");
+  
+//Pausa o programa após executar 
+  system("pause");
 
 }
